12b72e46c4a24ea598c6426baa8903a91a5f7bbb
Lucy <lucymidgley13@gmail.com>
2020-01-16 03:40:26 +0000

    Linted verison

--- fetcher.js
+++ fetcher.js
@@ -10,32 +10,31 @@ const rl = readline.createInterface({
 
 
 request(args[2], (error, response, body) => {
-  if(error){
-   return console.log('error: ', error);
+  if (error) {
+    return console.log('error: ', error);
   }
-  if(!fs.existsSync(args[3])){
-    return console.log("ERROR: Invalid file path")
-  }
-  else if(fs.existsSync(args[3]) && args[2]){
+  if (!fs.existsSync(args[3])) {
+    return console.log("ERROR: Invalid file path");
+  } else if (fs.existsSync(args[3]) && args[2]) {
     rl.question("The file already exists, overwrite Y/N?", (answer) => {
-      if(answer === "Y"){
-        writing(body, args[3])
-    }
-    else if (answer === "N"){
-      console.log("Cannot write file")
-  }});
-} else{
-  writing(body, args[3])
+      if (answer === "Y") {
+        writing(body, args[3]);
+      } else if (answer === "N") {
+        console.log("Cannot write file");
+      }
+    });
+  } else {
+    writing(body, args[3]);
 
-}
+  }
 });
 
-const writing = function(file, path) { 
+const writing = function(file, path) {
   fs.writeFile(path, file, (err) =>{
-    const stats = fs.statSync(path)
-const fileSizeInBytes = stats["size"]
+    const stats = fs.statSync(path);
+    const fileSizeInBytes = stats["size"];
     if (err) throw err;
-    console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ${path}`)
-})
-}
+    console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ${path}`);
+  });
+};
 
